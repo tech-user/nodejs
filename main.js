86c7bddc9112d56e3453613317b8f59198f9823e
Techuser <repo@techuser.in>
2019-08-23 00:21:04 +0530

    Create main.js
    Created main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+    // send the HTTP header
+    //HTTP Satus : 200 : OK
+    // Content Type : text/plain
+
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
+
+    // send the response body as "Hello, World"
+
+    response.end('Hello, World\n');
+}).listen(8081);
+
+//Console will print the message
+
+console.log('Server running at http://127.0.0.1:8081/');
